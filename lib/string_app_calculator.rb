2eb3067110e48b4485aa937f3bb957799736ed42
Rakesh Kumar <rajujack1007@gmail.com>
2024-06-05 17:43:52 +0530

    Added string app cal class and rspec file

---
+++ lib/string_app_calculator.rb
@@ -0,0 +1,5 @@
+class StringAppCalculator
+  def add(input)
+    0
+  end
+end
---
+++ spec/lib/string_app_calculator_spec.rb
@@ -0,0 +1,7 @@
+# spec/lib/string_calculator_spec.rb
+require 'rails_helper'
+require 'string_app_calculator'
+
+RSpec.describe StringAppCalculator do
+
+end
